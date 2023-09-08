518e858ee3f41642e367614bff80275d681d4a60
Avi Wagner <ajw127@duke.edu>
2023-09-08 14:55:37 -0400

    changed file name to default

--- src/Person201Farthest.java
+++ src/Person201Farthest.java
@@ -1,10 +1,10 @@
 public class Person201Farthest {
     public static void main(String[] args) throws Exception {
-        String file = "/Users/swagner/Documents/Duke Fall '23/CS 201/Projects/p0-201fall23/data/large.txt";
-        Person201[] people = Person201Utilities.readFile(file);
+        String file = "data/large.txt";
         double max = 0;
         Person201 a = null;
         Person201 b = null;
+        Person201[] people = Person201Utilities.readFile(file);
         for (Person201 p : people) {
             for (Person201 l : people){
                double d = p.distanceFrom(l);
